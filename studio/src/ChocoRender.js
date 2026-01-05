3a4762959a6faae11808bb9bf7a4a231ac05f4e3
Becca <codergal6502@gmail.com>
2026-01-04 19:20:41 -0500

    Removed the sequence diagram from the image server application from the Studio application.

--- studio/src/ChocoRender.js
+++ studio/src/ChocoRender.js
@@ -56,44 +56,6 @@ class ChocoWorkspaceRenderer {
             if (onComplete) onComplete(dataUrl);
         });
     }
-
-    // All of this will be done in the native application.
-    // URLs: l = layout, i = instance
-    // chat  streamer  obs html             firebot        ChocoServer          Workspace  Server Mutex Mem  ChocoRenderLayout
-    // ----  --------  -------------------  -------        -----------          ---------  ----------------  -----------------
-    //   |     |           |                    |                |                    |            |                 |
-    //   |     |--------------- load JSON file ----------------> |                    |            |                 |
-    //   |     | <-----------------------------------------------|                    |            |                 |
-    //   |     |           |                    |                |                    |            |                 |
-    //   |     |           |-----GET /l/<uuid>/i text/html-----> |                    |            |                 |
-    //   |     |           | <----------text/html----------------|                    |            |                 |
-    //   |     |           |                    |                |                    |            |                 |
-    //   |     |           |-----GET /l/<uuid>/i image/png-----> |                    |            |                 |
-    //   |     |           |                    |                |--get layout uuid-> |            |                 |
-    //   |     |           |                    |                | <-----layout-------|            |                 |
-    //   |     |           |                    |                |                    |            |                 |
-    //   |     |           |                    |                |---------get variables---------> |                 |
-    //   |     |           |                    |                | <------------variables----------+                 |
-    //   |     |           |                    |                |                    |            |                 |
-    //   |     |           |                    |                |----------layout, variables----------------------> |
-    //   |     |           |                    |                | <-------------------PNG---------------------------|
-    //   |     |           | <----------image/png----------------|                    |            |                 |
-    //   |     |           |                    |                |                    |            |                 |
-    //   |-----------chat event --------------> |                |                    |            |                 |
-    //   |     |           |                    |-set variables->|                    |            |                 |
-    //   |     |           |                    |                |---------set variables---------> |                 |
-    //   |     |           |                    |                | <-------------------------------+                 |
-    //   |     |           | <--------web socket message---------|                    |            |                 |
-    //   |     |           |-----GET /l/<uuid>/i image/png-----> |                    |            |                 |
-    //   |     |           |                    |                |--get layout uuid-> |            |                 |
-    //   |     |           |                    |                | <-----layout-------|            |                 |
-    //   |     |           |                    |                |                    |            |                 |
-    //   |     |           |                    |                |---------get variables---------> |                 |
-    //   |     |           |                    |                | <------------variables----------+                 |
-    //   |     |           |                    |                |                    |            |                 |
-    //   |     |           |                    |                |----------layout, variables----------------------> |
-    //   |     |           |                    |                | <-------------------------------------------------|
-    //   |     |           | <----------image/png----------------|                    |            |                 |
 }
 
 export { ChocoWorkspaceRenderer };
