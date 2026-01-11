6255cb4de619f82d74e1cf4889d75e767594c2c5
Becca <codergal6502@gmail.com>
2026-01-11 13:37:01 -0500

    Added configuration and tile sheet PNGs to ZIP file export.

--- studio/src/ZipDownloader.jsx
+++ studio/src/ZipDownloader.jsx
@@ -1,33 +1,48 @@
 import { ChocoWorkspaceRenderer } from "./ChocoRender";
 import { ChocoStudioWorkspace } from "./ChocoStudio";
-import { BlobReader, BlobWriter, ZipWriter } from "@zip.js/zip.js";
+import { BlobReader, BlobWriter, Data64URIReader, TextReader, ZipWriter } from "@zip.js/zip.js";
 
 /**
  * @param {ChocoStudioWorkspace} workspace
  * @return {Promise}
  */
 const downloadZip = (workspace) => {
+    const invalidCharRegEx = /[<>:""\\\/|?*]/g;
     const renderer = new ChocoWorkspaceRenderer(workspace);
 
-    const /** @type {Array.<Promise.<{name:String, reader:FileReader}>>} */ pngPromises = [];
+    const /** @type {Array.<Promise.<{name:String, reader:FileReader}>>} */ readerPromises = [];
 
     for (const layout of workspace.layouts) {
-        pngPromises.push(
+        readerPromises.push(
             new Promise(resolve =>
                 renderer.generateLayoutImageBlob(layout.id).then((blob) => {
                     const reader = new BlobReader(blob);
-                    resolve({ name: `${layout.name.replaceAll(/[<>:""\\\/|?*]/g, '-')}.png`, reader: reader });
+                    resolve({ name: `Layouts/${layout.name.replaceAll(invalidCharRegEx, '_')}.png`, reader: reader });
                 })
             )
         )
     };
 
+    readerPromises.push(
+        new Promise(resolve =>
+            resolve({ name: `${workspace.workspaceName.replace(invalidCharRegEx, '_')}.json`, reader: new TextReader(JSON.stringify(workspace)) })
+        )
+    );
+
+    for (const tileSheet of workspace.tileSheets) {
+        readerPromises.push(
+            new Promise(resolve =>
+                resolve({ name: `Tile Sheets/${tileSheet.name.replace(invalidCharRegEx, '_')}.png`, reader: new Data64URIReader(tileSheet.imageDataUrl) })
+            )
+        )
+    }
+
     const zipFileWriter = new BlobWriter();
     const zipWriter = new ZipWriter(zipFileWriter);
 
-    Promise.all(pngPromises).then((pngNamePairs) => {
-        return Promise.all(pngNamePairs.map(pngNamePair => new Promise(resolve => {
-            zipWriter.add(pngNamePair.name, pngNamePair.reader).then(() => resolve())
+    Promise.all(readerPromises).then((filePairs) => {
+        return Promise.all(filePairs.map(filePair => new Promise(resolve => {
+            zipWriter.add(`${filePair.name}`, filePair.reader).then(() => resolve())
         })))
     }).then(() => {
         zipWriter.close();
